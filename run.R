e82b3f79f9516e7f6b56733f06478d54bb59b816
Evelyn Cai <evelyncai@college.harvard.edu>
2020-07-08 14:25:37 -0500

    Add files via upload

---
+++ run.R
@@ -0,0 +1,12 @@
+# obtain port argument (if given)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) == 0) {
+        port <- "12000"
+} else if (length(args) == 1) {
+  port <- args[1]
+}
+rmarkdown::run(shiny_args = list(host = '0.0.0.0', port = as.numeric(port)))
+print(paste("Listening on port", port))
+
